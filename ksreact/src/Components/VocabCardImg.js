2b8c81fe0551975d540886d35e06b266bcdca4ff
nitrogendragon <dirtboy4fun@gmail.com>
2020-02-13 01:11:21 -0600

    fixed issue with vocabimg sizing and overflow, fixed pizza img not existing issue

--- ksreact/src/Components/VocabCardImg.js
+++ ksreact/src/Components/VocabCardImg.js
@@ -4,7 +4,7 @@ export default function VocabCardImg(props) {
     return (
         <div className='vocab-card'>
             {/* We always have to use this process.env.PUBLIC_URL + {props.imgUrl} to get our images to load if we want to be able to access by filename instead of doing tons of imports */}
-            <img src={process.env.PUBLIC_URL + props.imgUrl} alt=''></img>
+            <img style={{height: '100%', width: '100%'}} src={process.env.PUBLIC_URL + props.imgUrl} alt=''></img>
 
         </div>
     )
