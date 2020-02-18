3937233dbc3835831f5f4f917918e242bfefef9b
nitrogendragon <dirtboy4fun@gmail.com>
2020-02-18 16:11:29 -0600

    Filter dropdown changes the booleans on click now

--- ksreact/src/Components/Vocab/VocabSearch/VocabSearchBar.js
+++ ksreact/src/Components/Vocab/VocabSearch/VocabSearchBar.js
@@ -4,12 +4,22 @@ export default function VocabSearchBar(props) {
     const [acceptEnglish, setAcceptEnglish] = useState(true)
     const [acceptKanji, setAcceptKanji] = useState(true)
     const [acceptHiraganaRomaji, setAcceptHiraganaRomaji] = useState(true)
+    let activeFilters;
+    function handleFilter(stateSetter,e){
+        activeFilters = 0;
+        // if we are going to end up making no searches possible we wont change
+        acceptKanji ? activeFilters += 1 : activeFilters += 0
+        acceptHiraganaRomaji ? activeFilters += 1 : activeFilters += 0
+        acceptEnglish ? activeFilters += 1 : activeFilters +=0
+         stateSetter(e.target.checked) 
+    }
+    
     function handleSearch(){
         props.Search()
     }
-    function handleFilter(checked){
-        checked = !checked
-    }
+
+    
+    
     return (
         <div className="search-bar-container">
             <div className="search-bar">
@@ -20,14 +30,30 @@ export default function VocabSearchBar(props) {
                 <div className="filter-dropdown">
                     <button className="filter-dropdown-button">FILTER</button>
                     <div className="filter-dropdown-content">
-                        
-                        <input type = "checkbox" checked = "checked" value = {true} onChange={handleFilter}></input><p>English</p>
-                        <input type = "checkbox" checked = "checked" value = {true} onChange={handleFilter}></input>Kanji
-                        <input type = "checkbox" checked = "checked" value = {true} onChange={handleFilter}></input>Hiragana / romaji
-                        
+                        <label className="two-item-a-row-grid">
+                            <input 
+                                type = "checkbox" 
+                                checked = {acceptEnglish} 
+                                onChange={e => handleFilter(setAcceptEnglish, e)}>
+                            </input><p>English</p>
+                            <input 
+                                type = "checkbox" 
+                                checked = {acceptKanji}                              
+                                onChange={e => handleFilter(setAcceptKanji, e)}>
+                            </input><p>Kanji</p>
+                            <input 
+                                type = "checkbox" 
+                                checked = {acceptHiraganaRomaji}
+                                onChange={e => handleFilter(setAcceptHiraganaRomaji, e)}>
+                            </input><p>Hiragana / Romaji</p>
+                        </label>
                     </div>
                 </div>
             </div>
+            <p>{acceptEnglish.toString()}</p>
+            <p>{acceptHiraganaRomaji.toString()}</p>
+            <p>{acceptKanji.toString()}</p>
         </div>
+        
     )
 }
--- ksreact/src/Components/Vocab/VocabSearch/VocabSearchContainer.js
+++ ksreact/src/Components/Vocab/VocabSearch/VocabSearchContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import VocabSearchBar from './VocabSearchBar'
 export default function VocabSearchContainer() {
+    
     return (
         <div>
             {/* Search Bar */}
